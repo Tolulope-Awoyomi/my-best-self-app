824515fb21e2525e62edd0dc97ab00a9e9b960c7
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-05-30 23:02:02 -0400

    Send values and callbacks as props between Affirmation List and Affirmation Detail

--- src/components/AffirmationList.js
+++ src/components/AffirmationList.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AffirmationDetail from './AffirmationDetail';
 import AffirmationForm from './AffirmationForm';
 
 function AffirmationList() {
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const [affirmations, setAffirmations] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/affirmations")
+        .then ((r) => r.json())
+        .then ((affirmations) => setAffirmations(affirmations))
+    }, [])
+
+    function handleCategoryChange(category) {
+        setSelectedCategory(category);
+    }
+
+    function handleAddAffirmation(newAffirmation){
+        setAffirmations([...affirmations, newAffirmation])
+    }
+
+    const displayAffirmations = affirmations.filter((affirm) => {
+        if (selectedCategory === "All") return true;
+
+        return affirm.category === selectedCategory;
+    })
+
   return (
-    <>
-    <div>AffirmationList</div>
-    <AffirmationDetail />
+    <div className="AffirmationList">
+        <ul className='Affirmations'>
+            {displayAffirmations.map((affirm) => (
+                <AffirmationDetail 
+                    key={affirm.id}
+                    affirmation={affirm}/>
+            ))}
+        </ul>
+
     <AffirmationForm />
-    </>
+    </div>
   )
 }
 
