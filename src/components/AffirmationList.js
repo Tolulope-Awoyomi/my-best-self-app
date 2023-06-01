1e47a52768c6de644f1b40615293fcbe65db9fc2
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-05-31 22:00:53 -0400

    Include a search functionality to Affirmation List

--- src/components/AffirmationList.js
+++ src/components/AffirmationList.js
@@ -4,68 +4,82 @@ import AffirmationForm from './AffirmationForm';
 import Filter from './Filter';
 
 function AffirmationList() {
-    const [selectedCategory, setSelectedCategory] = useState("All");
-    const [affirmations, setAffirmations] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [affirmations, setAffirmations] = useState([]);
+  const [searchAffirm, setSearchAffirm] = useState('');
 
-    useEffect(() => {
-        fetch("http://localhost:3000/affirmations")
-        .then ((r) => r.json())
-        .then ((affirmations) => setAffirmations(affirmations))
-    }, [])
+  useEffect(() => {
+    fetch('http://localhost:3000/affirmations')
+      .then((r) => r.json())
+      .then((affirmations) => setAffirmations(affirmations));
+  }, []);
 
-    function handleCategoryChange(category) {
-        setSelectedCategory(category);
-    }
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
 
-    function handleAddAffirmation(newAffirmation){
-        setAffirmations([...affirmations, newAffirmation])
-    }
+  function handleAddAffirmation(newAffirmation) {
+    setAffirmations([...affirmations, newAffirmation]);
+  }
 
-    function handleUpdateAffirmation(updatedAffirm) {
-        const updatedAffirms = affirmations.map((affirm) => {
-            if (affirm.id === updatedAffirm.id) {
-                return updatedAffirm;
-            } else {
-                return affirm;
-            }
-        });
-        setAffirmations(updatedAffirms)
-    }
+  function handleUpdateAffirmation(updatedAffirm) {
+    const updatedAffirms = affirmations.map((affirm) => {
+      if (affirm.id === updatedAffirm.id) {
+        return updatedAffirm;
+      } else {
+        return affirm;
+      }
+    });
+    setAffirmations(updatedAffirms);
+  }
 
-    function handleDeleteAffirmation(deletedAffirm) {
-        const updatedAffirms = affirmations.filter((affirmation) => affirmation.id !== deletedAffirm.id)
-        setAffirmations(updatedAffirms)
-    }
+  function handleDeleteAffirmation(deletedAffirm) {
+    const updatedAffirms = affirmations.filter(
+      (affirmation) => affirmation.id !== deletedAffirm.id
+    );
+    setAffirmations(updatedAffirms);
+  }
 
-    const displayAffirmations = affirmations.filter((affirm) => {
-        if (selectedCategory === "All") return true;
+  const displayAffirmations = affirmations
+    .filter((affirm) => {
+      if (selectedCategory === 'All') return true;
 
-        return affirm.category === selectedCategory;
+      return affirm.category === selectedCategory;
     })
+    .filter((affirm) => {
+      if (searchAffirm.trim() === '') return true;
+
+      return affirm.statement.toLowerCase().includes(searchAffirm.toLowerCase());
+    });
 
   return (
     <div className="AffirmationList">
-        <AffirmationForm onAddAffirmation={handleAddAffirmation}/>
-        
-        <Filter 
-            category={selectedCategory}
-            onCategoryChange={handleCategoryChange}/>   
+      <AffirmationForm onAddAffirmation={handleAddAffirmation} />
 
-        <ul className='Affirmations'>
-            {displayAffirmations.map((affirm) => (
-                <AffirmationDetail 
-                    key={affirm.id}
-                    affirm={affirm}
-                    id={affirm.id}
-                    statement={affirm.statement}
-                    category={affirm.category}
-                    onUpdateAffirm={handleUpdateAffirmation}
-                    onDeleteAffirm={handleDeleteAffirmation}
-                    />
-            ))}
-        </ul>
+      <Filter category={selectedCategory} onCategoryChange={handleCategoryChange} />
+
+      <input
+        type="text"
+        placeholder="Search Affirmations"
+        value={searchAffirm}
+        onChange={(e) => setSearchAffirm(e.target.value)}
+      />
+
+      <ul className="Affirmations">
+        {displayAffirmations.map((affirm) => (
+          <AffirmationDetail
+            key={affirm.id}
+            affirm={affirm}
+            id={affirm.id}
+            statement={affirm.statement}
+            category={affirm.category}
+            onUpdateAffirm={handleUpdateAffirmation}
+            onDeleteAffirm={handleDeleteAffirmation}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default AffirmationList
+export default AffirmationList;
