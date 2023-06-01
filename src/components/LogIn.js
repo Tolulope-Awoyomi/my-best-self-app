f0f2f6350628784d8f021ae6fade31c99e9c3797
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-05-31 22:28:42 -0400

    Set up Login component

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-function LogIn({ isLoggedIn }) {
-    if (!isLoggedIn) return <Redirect to="login"/>
-    
+function Login({ setIsLoggedIn }) {
+  const history = useHistory();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setIsLoggedIn(true);
+
+    history.push("/home");
+  }
+
   return (
-    <div>LogIn</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
 
-export default LogIn
+export default Login;
